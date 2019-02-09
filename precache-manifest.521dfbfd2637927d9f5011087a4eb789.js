8190f6f24ad7050b800f9cbacb59be1c0219ff6c
Slava Afanasev <vyaches.afanas@yandex.ru>
2019-02-09 15:07:44 +0400

    deploy

---
+++ precache-manifest.521dfbfd2637927d9f5011087a4eb789.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/fonts/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2dae0f84049beb6993780437cea2476b",
+    "url": "/sass/app.scss"
+  },
+  {
+    "revision": "d229eb0422f9f85aef5c",
+    "url": "/js/chunk-vendors.80cce23c.js"
+  },
+  {
+    "revision": "4c9ff556a909b78b65b2",
+    "url": "/js/app.7ab8f6ee.js"
+  },
+  {
+    "revision": "b3f7b86ea919061b8487511d85c4ebb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/img/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/fonts/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/fonts/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/fonts/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "d229eb0422f9f85aef5c",
+    "url": "/css/chunk-vendors.33dd283d.css"
+  },
+  {
+    "revision": "4c9ff556a909b78b65b2",
+    "url": "/css/app.7b8e78d7.css"
+  }
+];
